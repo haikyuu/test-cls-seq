24ca80f14de1d9965e0528ac93a24dd02c8e68d5
Alaoui Abdellah <als.abdellah@gmail.com>
2015-05-04 16:26:17 +0100

    instanciating sequelize conection in a namespace

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var config = require('./config.json');
-
+var cls = require('continuation-local-storage');
+var patcher = require('cls-bluebird');
 
 var path = require('path');
 var lodash = require('lodash');
@@ -8,40 +9,47 @@ var fs = require('fs');
 var Sequelize = require('sequelize');
 
 
-var sequelize = new Sequelize(config.MYSQL_DB, config.MYSQL_USER, config.MYSQL_PASSWORD, {
-  logging: false,
-  host: config.MYSQL_URI,
-  port: config.MYSQL_PORT
-});
+var ns = cls.createNamespace('index');
+patcher(ns);
 
-var db = {};
+ns.run(function() {
+
+  var sequelize = new Sequelize(config.MYSQL_DB, config.MYSQL_USER, config.MYSQL_PASSWORD, {
+    logging: false,
+    host: config.MYSQL_URI,
+    port: config.MYSQL_PORT
+  });
+  ns.set('number', 888888);
 
 
-sequelize.sync({
-  force: false
-}).then(function() {
-  console.log('database sync.');
-});
+  var db = {};
 
-fs.readdirSync(__dirname)
-  .filter(function(file) {
-    return (/\w+\.js$/.test(file)) && (file !== 'index.js')
-  }).forEach(function(file) {
-    console.log(file);
-    var model = sequelize.import(path.join(__dirname, file));
-    console.log(model.name);
-    db[model.name] = model;
 
+  sequelize.sync({
+    force: false
+  }).then(function() {
+    console.log('database sync.');
   });
 
-Object.keys(db).forEach(function(modelName) {
-  console.log(modelName);
-  if ('associate' in db[modelName]) {
-    db[modelName].associate(db);
-  }
-});
+  fs.readdirSync(__dirname)
+    .filter(function(file) {
+      return (/\w+\.js$/.test(file)) && (file !== 'index.js')
+    }).forEach(function(file) {
+      console.log(file);
+      var model = sequelize.import(path.join(__dirname, file));
+      console.log(model.name);
+      db[model.name] = model;
 
-module.exports = lodash.extend({
-  sequelize: sequelize,
-  Sequelize: Sequelize
-}, db);
+    });
+
+  Object.keys(db).forEach(function(modelName) {
+    console.log(modelName);
+    if ('associate' in db[modelName]) {
+      db[modelName].associate(db);
+    }
+  });
+  module.exports = lodash.extend({
+    sequelize: sequelize,
+    Sequelize: Sequelize
+  }, db);
+});
